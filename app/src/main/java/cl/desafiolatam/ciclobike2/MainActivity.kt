bdbd54b2dc650daebaf43c69f71d3643969a6205
AlejandroPerezGarcia <alejandro8001@gmail.com>
2020-08-14 15:43:58 -0400

    inicio de colocar boton y spiner

--- app/src/main/java/cl/desafiolatam/ciclobike2/MainActivity.kt
+++ app/src/main/java/cl/desafiolatam/ciclobike2/MainActivity.kt
@@ -2,10 +2,39 @@ package cl.desafiolatam.ciclobike2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ciromine.ciclobike.SetupCiclovias
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val setupCiclovias = SetupCiclovias().init()
+       // val myAdapter = CycloAdapter(setupCiclovias)
+       // cyclo_recicler.adapter = CycloAdapter(SetupCiclovias().init())
+      //  cyclo_recicler.adapter = myAdapter
+
+        val listOf = listOf( "Las Condes",
+            "La Reina",
+            "Ñuñoa",
+            "Macul",
+            "Providencia")
+
+
+
+
     }
+
+
+
+
+
+    fun SinFiltro (view : View){
+
+        cyclo_recicler.adapter = CycloAdapter(SetupCiclovias().init())
+    }
+
 }
